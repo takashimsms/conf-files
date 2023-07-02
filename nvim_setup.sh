26d1d15522e22b0b2df77e3e5c1c27fbc9ba1fbb
takashimsms <94922630+takashimsms@users.noreply.github.com>
2023-07-02 20:43:38 +0900

    Update nvim_setup.sh

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -9,6 +9,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cd ~
 git clone https://github.com/takashimsms/conf-files
+mkdir ~/.config
 mkdir ~/.config/nvim
 mv ~/conf-files/init.vim ~/.config/nvim/
-npm install nodejs
+sudo npm install -g n
+sudo n install latest
