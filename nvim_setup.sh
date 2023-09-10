86cdbf741a26b0b0d8e4aae13acae2bc2b3e23db
takashimsms <94922630+takashimsms@users.noreply.github.com>
2023-09-10 19:13:22 +0900

    Update nvim_setup.sh

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,16 +1,16 @@
 cd `dirname $0`
 cd ~
 sudo apt-get update
-sudo apt-get install ninja-build gettext cmake unzip curl npm -y
+sudo apt-get install ninja-build gettext cmake unzip curl -y
+# sudo apt-get install npm -y
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cd ~
-git clone https://github.com/takashimsms/conf-files
 mkdir ~/.config
 mkdir ~/.config/nvim
 mv ~/conf-files/init.vim ~/.config/nvim/
-sudo npm install -g n
-sudo n install latest
+# sudo npm install -g n
+# sudo n install latest
